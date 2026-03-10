a6abf70c40e4a419bb9ef4e688fa2f7e8077f32d
Abhijeet Verma <13599158+abhijeet029@users.noreply.github.com>
2026-03-10 21:57:30 +0530

    Refactor Cart component with item quantity controls

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,75 +1,120 @@
 import React, { useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../components/context/StoreContext'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
 
 const Cart = () => {
 
-  const {cartItems, food_list, removeFromCart, getTotalCartAmount, url} = useContext(StoreContext);
+  const { cartItems, food_list, addToCart, removeFromCart, getTotalCartAmount, url } = useContext(StoreContext)
+  const navigate = useNavigate()
 
-  const navigate = useNavigate();
   return (
     <div className='cart'>
+
       <div className="cart-items">
+
         <div className="cart-items-title">
-          <p>Itmes</p>
+          <p>Items</p>
           <p>Title</p>
           <p>Price</p>
           <p>Quantity</p>
           <p>Total</p>
           <p>Remove</p>
         </div>
+
         <br />
         <hr />
-        {food_list.map((item,index)=>{
-          if(cartItems[item._id]>0){
-            return <div>
-              <div className="cart-items-title cart-items-item">
-              <img src={url+'/images/'+item.image} alt="" />
-              <p>{item.name}</p>
-              <p>${item.price}</p>
-              <p>{cartItems[item._id]}</p>
-              <p>${item.price*cartItems[item._id]}</p>
-              <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
-            </div>
-            <hr />
-            </div>
 
-            
+        {food_list.map((item) => {
+
+          if (cartItems[item._id] > 0) {
+            return (
+              <div key={item._id}>
+
+                <div className="cart-items-title cart-items-item">
+
+                  <img src={url + '/images/' + item.image} alt="" />
+
+                  <p>{item.name}</p>
+
+                  <p>${item.price}</p>
+
+                  {/* Adding this logic to control card + - requirement */}
+                  <div className="cart-quantity">
+                    <button onClick={() => removeFromCart(item._id)}>-</button>
+                    <span>{cartItems[item._id]}</span>
+                    <button onClick={() => addToCart(item._id)}>+</button>
+                  </div>
+
+                  <p>${item.price * cartItems[item._id]}</p>
+
+                  <p onClick={() => removeFromCart(item._id)} className='cross'>x</p>
+
+                </div>
+
+                <hr />
+
+              </div>
+            )
           }
+
+          return null
         })}
+
       </div>
+
       <div className="cart-bottom">
+
         <div className="cart-total">
+
           <h2>Cart Total</h2>
+
           <div>
+
             <div className="cart-total-detail">
               <p>Subtotal</p>
               <p>${getTotalCartAmount()}</p>
             </div>
+
             <hr />
+
             <div className="cart-total-detail">
               <p>Delivery Fee</p>
-              <p>${getTotalCartAmount()===0?0:2}</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
+
             <hr />
+
             <div className="cart-total-detail">
               <b>Total</b>
-              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
-            </div> 
+              <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
+            </div>
+
           </div>
-          <button onClick={()=> navigate('/order')}>PROCEED TO CHECKOUT</button>
+
+          <button onClick={() => navigate('/order')}>
+            PROCEED TO CHECKOUT
+          </button>
+
         </div>
+
         <div className="cart-promocode">
+
           <div>
+
             <p>If you have a promo code, enter it here</p>
+
             <div className='cart-promocode-input'>
-              <input type="text" placeholder='Promo Code'/>
+              <input type="text" placeholder='Promo Code' />
               <button>Submit</button>
             </div>
+
           </div>
+
         </div>
+
       </div>
+
     </div>
   )
 }
