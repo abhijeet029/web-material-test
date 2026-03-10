dac3ea819f8f319263670254286e6bf498d80a57
Abhijeet Verma <13599158+abhijeet029@users.noreply.github.com>
2026-03-10 21:56:43 +0530

    Change response to use status codes for errors
    user status code update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,13 +10,13 @@ const loginUser = async (req, res) => {
     const user = await userModel.findOne({ email });
 
     if (!user) {
-      return res.json({ success: false, message: "User does not exist" });
+      return res.status(401).json({ success: false, message: "User does not exist" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.json({ success: false, message: "Invalid credentials" });
+      return res.status(401).json({ success: false, message: "Invalid credentials" });
     }
 
     const token = createToken(user._id);
